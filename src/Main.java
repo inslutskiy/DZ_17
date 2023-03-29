905d06d754c14de9b2894423d4b58d63858c7a98
inslutskiy <inslutskiy@gmail.com>
2023-03-29 12:44:16 +0300

    DZ v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Person> map = new ArrayList<>();
-        map.add(new Person("Иван", "Иванов-Петров", 10));
-        map.add(new Person("Илья", "Иванов-Петров-Зайцев", 12));
-        map.add(new Person("Оля", "Серова-Зайцев", 14));
-        map.add(new Person("Ира", "Зайцев", 22));
-        map.add(new Person("Ксения", "Львова-Носкова-Нам", 15));
 
-        for (int i = 0; i < map.size(); i++) {
-            System.out.println(map.get(i));
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Иван", "Иванов-Петров", 10));
+        people.add(new Person("Илья", "Иванов-Петров-Зайцев", 12));
+        people.add(new Person("Оля", "Серова-Зайцев", 14));
+        people.add(new Person("Ира", "Зайцев", 22));
+        people.add(new Person("Ксения", "Львова-Носкова-Нам", 15));
+
+        for (Person person : people) {
+            System.out.println(person);
         }
 
-        Collections.sort(map);
+        Predicate<Person> predicate = person -> person.age < 18;
+
+        people.removeIf(predicate);
+
         System.out.println("==========================");
-        for (int i = 0; i < map.size(); i++) {
-            System.out.println(map.get(i));
+        for (Person person : people) {
+            System.out.println(person);
         }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,9 @@
-public class Person implements Comparable<Person>{
-    private String name;
-    private String surname;
-    private int age;
-    private String[] surnamel;
-    private int surnameLength;
+public class Person {
+    public String name;
+    public String surname;
+    public int age;
+    public String[] surnamel;
+    public int surnameLength;
 
     public Person(String name, String surname, int age){
         this.name = name;
@@ -26,16 +26,5 @@ public class Person implements Comparable<Person>{
     @Override
     public String toString() {
         return name + " " + surname + " " + age + " лет";
-    }
-
-    @Override
-    public int compareTo(Person o) {
-        if (surnameLength > o.surnameLength) {
-            return -1;
-        } else if (surnameLength < o.surnameLength) {
-            return 1;
-        } else {
-            return o.age - age;
-        }
     }
 }
